48ffc0f9ee40c1830cf7fba9b7c4fa1c935406b7
HMoradiRad <moradirad007@gmail.com>
2020-03-22 13:36:46 +0430

    add jquery to app

--- Server/server.js
+++ Server/server.js
@@ -1,50 +1,44 @@
 const path = require('path');
 const express = require('express')
-const publicpath = path.join(__dirname,'../public')
-const port = process.env.PORT || 3000;
 const socketIO = require('socket.io')
 const http = require('http');
+var {generateMessage} = require('./utils/message')
+
+const publicPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000;
 
 var app = express();
-var server = http.createServer(app);
+var server = http.createServer(app)
 var io = socketIO(server)
 
-io.on('connection',(socket)=>{
-    console.log("New user connection")
+//Broadcasting event
 
-    socket.emit('newMessage',{
-        from:"admin",
-        text:"wellcome to chat ",
-        createAt: new Date().getTime()
+io.on('connection', (socket)=>{
+    console.log('New User Connection!')
+
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
+
+    socket.on('createMessage', (message)=>{
+        console.log('createMessage', message)
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
     })
-
-    socket.broadcast.emit('newMessage',{
-        from:"admin",
-        text:"new user joined",
-        createAt:new Date().getTime()
-    })
-
-    // socket.emit('newMessage',{
-    //     from : "kakero",
-    //     text:"salam khobi chetori??",
-    //     createAt:123
-    // })
-   socket.on('createMessage',(message)=>{
-       console.log("create message : ",message)
-        io.emit('newMessage',{
-            from : message.from,
-            text: message.text,
-            createAt : new Date().getTime()
-        })
-   })
-    socket.on('disconnect',()=>{
-        console.log("disconnected ...!")
+    
+    socket.on('disconnect', ()=>{
+        console.log("User was disconnected!");
     })
 })
+app.use(express.static(publicPath))
 
-app.use(express.static(publicpath))
 
-
-server.listen(3000,()=>{
-    console.log(`server is up on ${port}`)
+server.listen(port, ()=>{
+    console.log(`Server is up on ${port}`)
 })
--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,30 @@
 var socket = io();
-socket.on('connect',function(){
-    console.log("conected to server ...");
-socket.emit('createMessage',{
-    from:"Hossein",
-    text:"salam dash hossien"
-    }) 
+
+socket.on('connect', function(){
+    console.log('Connected to server');
+
+    // socket.emit('createMessage', {
+    //     from: "masoud",
+    //     text: "Hey, it's work"
+    // })
 })
 
-socket.on('disconnect',function(){
-    console.log("disconnect plz try again later!");
+socket.on('disconnect', function(){
+    console.log('Disconnected from server');
 })
+socket.on('newMessage', function(message){
+    console.log('New Message', message)
 
-socket.on('newMessage',function(message){
-    console.log("new message ", message)
-});
+    var li = jQuery('<li></li>')
+    li.text(`${message.from}: ${message.text}`)
+
+    jQuery('#messages').append(li)
+})
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    })
+})
